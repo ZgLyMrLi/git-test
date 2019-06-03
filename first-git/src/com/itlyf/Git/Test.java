f46151c8dee5f49ceb1ababf3a0bfcc8fd6e3d21
MrLi <www.616729942@qq.com>
2019-06-03 09:20:31 +0800

    把本地工程添加到GIT

---
+++ first-git/src/com/itlyf/Git/Test.java
@@ -0,0 +1,12 @@
+package com.itlyf.Git;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+    }
+}
